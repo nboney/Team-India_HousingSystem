4ba4bf0ee47e96e9d5e4b69f87625b69be80b6db
LittlePickle <57980036+LittlePickle@users.noreply.github.com>
2020-10-18 17:35:11 -0400

    Add files via upload

---
+++ src/Document.java
@@ -0,0 +1,12 @@
+public class Document {
+    protected String firstName;
+    protected String lastName;
+    protected String currentAddress;
+    protected String phone;
+    protected String socialSecurityNumber;
+
+    public Document(){
+
+
+    }
+}
