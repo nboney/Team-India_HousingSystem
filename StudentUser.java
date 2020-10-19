e37983a26be4ac7025f96094b33589581c614c7d
Adam E <69927855+einsteia@users.noreply.github.com>
2020-10-19 19:42:56 -0400

    Update StudentUser.java

--- StudentUser.java
+++ StudentUser.java
@@ -1,16 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package housingsystem;
-
-import java.util.ArrayList;
-
-/**
- *
- * @author adameinstein
- */
 public class StudentUser {
     private String studentID;
     private String currentAddress;
