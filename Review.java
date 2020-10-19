89827024dd6ccd602ef37f245dba606284f00d8c
Adam E <69927855+einsteia@users.noreply.github.com>
2020-10-19 19:42:12 -0400

    Update Review.java

--- Review.java
+++ Review.java
@@ -1,14 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package housingsystem;
-
-/**
- *
- * @author adameinstein
- */
 public class Review {
     private StudentUser writer;
     private double rating;
