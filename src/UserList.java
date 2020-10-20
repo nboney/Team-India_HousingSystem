ce2c0ee920c1384e5b5fe4007984be1effa14aa2
Nick Boney <nboney@email.sc.edu>
2020-10-19 22:19:27 -0400

    Renamed HousingListList to HousingListings

---
+++ src/UserList.java
@@ -0,0 +1,15 @@
+
+public class UserList {
+	private Users users;
+	private ArrayList<RegisteredUser> userList;
+	
+	private UserList() {
+		userList = DataLoader.getUsers();
+	}
+	
+	public Users getInstance() {
+		if(users == null) {
+			users = new u
+		}
+	}
+}
