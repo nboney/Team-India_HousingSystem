df89e888eb1650086ad4706fd2d2db950b560bf1
Adam E <69927855+einsteia@users.noreply.github.com>
2020-10-21 18:00:49 -0400

    Update User.java
    added implementations

--- src/User.java
+++ src/User.java
@@ -1,15 +1,19 @@
 import java.util.ArrayList;
 
 public class User {
+    private ArrayList<HousingListing> search;
+    private ArrayList<HousingListing> tag;
     
     public User () {
+        this.search = search;
+        this.tag = tag;
     }
     
     public ArrayList<HousingListing> search() {
-        return null;
+        return this.search;
     }   
         
     public ArrayList<HousingListing> search(String tag) {
-        return null;    
+       return this.tag;    
     }    
 }
