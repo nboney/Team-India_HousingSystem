8aa808df1277425b0ba4be85b9c4c8f3b39d192e
Adam E <69927855+einsteia@users.noreply.github.com>
2020-10-21 18:16:56 -0400

    Update RegisteredUser.java
    added implementations

--- src/RegisteredUser.java
+++ src/RegisteredUser.java
@@ -7,8 +7,28 @@ public class RegisteredUser extends User {
     protected String email;
     protected int rating;
     protected int ratingCount;
+    protected RegisteredUser user;
+    private String firstName2;
+    private String lastName2;
+    private String username2;
+    private String password2;
+    private String email2;
+    private int parseInt;
+    private int parseInt2;
     
-    public RegisteredUser() {
+    public RegisteredUser(String firstName,String lastName,
+                          String username, String password,
+                          String phone,String email, int rating,
+                          int ratingCount) {
+        
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.phone = phone;
+        this.email = email;
+        this.rating = rating;
+        this.ratingCount = ratingCount;
     }
     
     /*
@@ -17,8 +37,17 @@ public class RegisteredUser extends User {
     public RegisteredUser(String firstName2, String lastName2, String username2, String password2, String email2,
                           int parseInt, int parseInt2) {
     // TODO Auto-generated constructor stub
+        this.firstName2 = firstName2;
+        this.lastName2 = lastName2;
+        this.username2 = username2;
+        this.password2 = password2;
+        this.email2 = email2;
+        this.parseInt = parseInt;
+        this.parseInt2 = parseInt2;     
   }
 
   public void leaveUserRating(RegisteredUser user, int rating) {
+      this.user = user;
+      this.rating = rating;
     }
 }
