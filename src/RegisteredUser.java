5554ecb7fd230ccdd3826eae7a3e355beed344e4
Nick Boney <nboney@email.sc.edu>
2020-10-22 07:19:42 -0400

    Updated Users class with implementation

--- src/RegisteredUser.java
+++ src/RegisteredUser.java
@@ -8,13 +8,7 @@ public class RegisteredUser extends User {
     protected int rating;
     protected int ratingCount;
     protected RegisteredUser user;
-    private String firstName2;
-    private String lastName2;
-    private String username2;
-    private String password2;
-    private String email2;
-    private int parseInt;
-    private int parseInt2;
+    
     
     public RegisteredUser(String firstName,String lastName,
                           String username, String password,
@@ -37,17 +31,21 @@ public class RegisteredUser extends User {
     public RegisteredUser(String firstName2, String lastName2, String username2, String password2, String email2,
                           int parseInt, int parseInt2) {
     // TODO Auto-generated constructor stub
-        this.firstName2 = firstName2;
-        this.lastName2 = lastName2;
-        this.username2 = username2;
-        this.password2 = password2;
-        this.email2 = email2;
-        this.parseInt = parseInt;
-        this.parseInt2 = parseInt2;     
+        this.firstName = firstName2;
+        this.lastName = lastName2;
+        this.username = username2;
+        this.password = password2;
+        this.email = email2;
+        this.rating = parseInt;
+        this.ratingCount = parseInt2;     
   }
 
   public void leaveUserRating(RegisteredUser user, int rating) {
       this.user = user;
       this.rating = rating;
     }
+  
+  public String getUsername() {
+	  return this.username;
+  }
 }
--- src/Users.java
+++ src/Users.java
@@ -16,11 +16,27 @@ public class Users {
 		return users;
 	}
 	
-	public boolean haveUser(String username) {
-		return true;
+	public boolean haveUser(String username, int index) {
+		/*for(int i = 0; i < userList.size(); ++i) {
+			RegisteredUser currUser = userList.get(i);
+			if(currUser.getUsername().contentEquals(username)) {
+				return true;
+			}
+		}*/
+		if(userList.get(index).getUsername().contentEquals(username)) {
+			return true;
+		}
+		
+		return false;
 	}
 	
 	public User getUser(String username) {
+		for(int i = 0; i < userList.size(); ++i) {
+			if(users.haveUser(username, i)) {
+				return userList.get(i);
+			}
+		}
+		
 		return null;
 	}
 }
