9575a42a0c3ed3c3569923c95bba1736a3e1cf3a
guzzarlk <70454751+guzzarlk@users.noreply.github.com>
2020-11-02 18:43:53 -0500

    Update LeaseAgreement.java

--- src/LeaseAgreement.java
+++ src/LeaseAgreement.java
@@ -1,27 +1,60 @@
-public class LeaseAgreement extends Document{
-    private String propertyAddress;
-    private String leaseLength;
-    private String monthlyRent;
-    private String termsOfAgreement;
-    private boolean agreeToTerms;
+import java.io.*;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.FileWriter;
 
-    public LeaseAgreement(String firstName, String lastName, String currentAddress, String phone,
-                          String socialSecurityNumber, String propertyAddress, int leaseLength,
-                          double monthlyRent){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.currentAddress = currentAddress;
-        this.phone = phone;
-        this.socialSecurityNumber = socialSecurityNumber;
-        this.propertyAddress = propertyAddress;
+public class LeaseAgreement { //extends Document {
+	private String landlord;
+	private String tenants = "";
+	private String propertyAddress;
+	private int num_bed;
+	private int num_bath;
+	private int sczip;
+    private int startdate;
+    private int enddate;
+    private int rent;
+    private String paymentAddress;
+    private int damages;
+    private String tenant1 = "";
+
+    public LeaseAgreement() {
+    	
     }
-
-    public boolean acceptTheTerms(){
-        return acceptTheTerms();
+	
+    public static void modifytxt(String fileName, String target, String replacement, String toFileName) throws IOException {
+    	
+    	//file should be local
+    	File oglease = new File("/Users/sonaguzzarlamudi/Desktop/LeaseAgreement.txt");
+		Scanner scanner = new Scanner(oglease);
+		FileWriter writer = new FileWriter("/Users/sonaguzzarlamudi/Desktop/NewLeaseAgreement.txt");
+		String line;
+		while(scanner.hasNextLine()) {
+			line = scanner.nextLine();
+			line = line.replaceAll(target, replacement);
+			writer.write(line);
+			//writer.newLine();
+		}
+		scanner.close();
+		writer.close();
     }
-
-    public String toString(){
-        return "";
+	
+    public static void main(String[] args) throws IOException {
+    	
+    	//user is supposed to put in landlord's name
+    	Scanner keyboard = new Scanner(System.in);
+        String newLL = keyboard.nextLine();
+    	modifytxt("c:/Users/sonaguzzarlamudi/Desktop/LeaseAgreement.txt", "LANDLOARD", ""+newLL+"",
+    			"d:/Users/sonaguzzarlamudi/Desktop/NewLease.txt");
+    	//if you take out newLL and put in anything, it should export to desktop
     }
-
 }
+
+    	
+    	
+    
