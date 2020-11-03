6b97ce1775edf9d550b3d45def94e55423e8edfc
Adam E <69927855+einsteia@users.noreply.github.com>
2020-11-02 20:47:45 -0500

    Update HousingSystem.java

--- src/HousingSystem.java
+++ src/HousingSystem.java
@@ -7,10 +7,12 @@ public class HousingSystem {
         reviews = Reviews.getInstance();
     }
     
+    //Returns true if item is found, and false otherwise
     public boolean StudentUserLogin(String username, String password) {
         return users.studentLogin(username, password);
     }
     
+    //Returns true if item is found, and false otherwise
     public boolean NoneStudentLogin(String username, String password) {
         return users.leaserLogin(username, password);
     }  
