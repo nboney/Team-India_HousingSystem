7c2ae303fee088f905cec81cf091bf32b57913f9
LittlePickle <57980036+LittlePickle@users.noreply.github.com>
2020-11-04 00:03:30 -0500

    Update HousingUI.java

--- src/HousingUI.java
+++ src/HousingUI.java
@@ -4,22 +4,31 @@ public class HousingUI {
     private static final String  WELCOME_MESSAGE = "Welcome to Team India Housing system";
     private static final String ChoiceByNumber = "Please choose by command number " ;
     private Scanner sc;
-    private String[] mainMenuOptions = {"Students", "None Student" ,"Quit"};
-    private String[] StudentLoginMenu = {"Sign in" ,"Sign up", "Back"};
-    private String[] NoneStudentLoginMenu = {"Sign in" ,"Sign up", "Back"};
+    private String[] MainMenuOptions = {"Log in as Student User","Login as Property Managers","Sign up","Quit"};
+    private String[] UserSingUpMenu = {"Sign Up as Student" , "Sign Up as Property Managers"};
+    private String[] StudentLoginMenu = {"Sign in"};
+    private String[] PropertyManagers = {"Sign in"};
     private String[] StudentMainMenu = {"Find a House", "Sign a Lease" ," Leave a Review", "Back to login"};
-    private String[] NoneStudentMainMenu = {"Upload a House" , "Sign a Lease"};
+    private String[] FindHouse = {"HouseListing","Housing Type" , "Amenities"};
+    private String[] PropertyManagersMainMenu = {"Upload a House" , "Sign a Lease", "Back to login"};
     private String [] HouseAmenities = {"Numbers of Bedrooms" , "Numbers of Bathrooms",
             "Allow pets", "Having a pool", "Included Washer and Dyer"};
     private User user;
+    private StudentUser studentUser;
+
 
 
     HousingUI(){
         sc = new Scanner(System.in);
         User user = new User();
         StudentUser studentUser = new StudentUser();
+        LeasingUser leasingUser = new LeasingUser();
         HousingListings housingListings = HousingListings.getInstance();
         Reviews reviews = Reviews.getInstance();
+        Amenities amenities = Amenities.getAmenities;
+        LeaseAgreement leaseAgreement = LeaseAgreement.getLeaseAgreement;
+        HousingType housingType = HousingType.getHousingType;
+
 
     }
 
@@ -27,20 +36,24 @@ public class HousingUI {
 
     public void run(){
         System.out.println(WELCOME_MESSAGE);
-            displayMenu(mainMenuOptions);
-            int command = getCommand(mainMenuOptions.length);
+            displayMenu(MainMenuOptions);
+            int command = getCommand(MainMenuOptions.length);
 
             switch (command){
-                case (1):
+                case (0):
                     StudentUserLogin();
                     break;
+                case (1):
+                    PropertyManagersLogin();
+                    break;
                 case (2):
-                    NoneStudentLogin();
+                    ;
                     break;
                 case (3):
-                    System.out.println("Bye!");
+                    System.out.println("Bye" + "\n");
+                    break;
                 default:
-                    System.out.println("Wrong choice!");
+                    System.out.println("Wrong choice!" + "\n");
                     break;
             }
 
@@ -56,9 +69,9 @@ public class HousingUI {
     }
 
     private int getCommand(int numCommands) {
-        System.out.println("Please Enter the command number");
-        String input = sc.nextLine();
-        int command = Integer.parseInt(input) - 1;
+        System.out.println("Please Enter the command number" + "\n");
+        sc = new Scanner(System.in);
+        int command = Integer.parseInt(String.valueOf(sc)) - 1;
         if(command >= 0 && command <= numCommands -1) return command;
 
         return -1;
@@ -72,21 +85,172 @@ public class HousingUI {
         displayMenu(StudentLoginMenu);
         command = getCommand(StudentLoginMenu.length);
 
-        switch (command){
-
+        switch (command) {
+            case (0):
+                StudentMainMenu();
+                break;
         }
 
 
         }
 
-    public void NoneStudentLogin(){
+    public void PropertyManagersLogin(){
         int command;
-        displayMenu(NoneStudentLoginMenu);
-        command = getCommand(NoneStudentLoginMenu.length);
+        displayMenu(PropertyManagers);
+        command = getCommand(PropertyManagers.length);
 
         }
 
-        public void
+     public void UserSingUp(){
+        int command;
+        displayMenu(UserSingUpMenu);
+        command = getCommand(UserSingUpMenu.length);
+
+        switch (command){
+            case (0):
+                NewStudent();
+                break;
+            case (1):
+                NewPropertyManagers();
+                break;
+
+            default:
+                System.out.println("Wrong choice!" + "\n");
+                break;
+        }
+     }
+
+    public void  NewStudent(){
+        System.out.println("Please enter your first name" + "\n");
+        String firstName = sc.nextLine();
+        System.out.println("Please enter your last name" + "\n");
+        String lastName = sc.nextLine();
+        System.out.println("Please enter your USC ID" + "\n");
+        String id = sc.nextLine();
+        System.out.println("Please enter your password" + "\n");
+
+        StudentUser newStudent = new StudentUser(firstName,lastName,id);
+        StudentUser studentUser = StudentUser.getStudents();
+        studentUser.addStudent(newStudent);
+        user = newStudent;
+
+        System.out.println("Welcome" + firstName + lastName);
+        StudentMainMenu();
+    }
+
+    public void NewPropertyManagers(){
+        System.out.println("Please enter your first name" + "\n");
+        String firstName = sc.nextLine();
+        System.out.println("Please enter your last name" + "\n");
+        String lastName = sc.nextLine();
+        System.out.println("Please enter your USC ID" + "\n");
+        String id = sc.nextLine();
+        System.out.println("Please enter your password" + "\n");
+
+        PropertyManagers propertyManagers = PropertyManagers.getPropertyManagers();
+        PropertyManagers.addPropertyManagers(newPropertyManagers);
+        user = newPropertyManagerst;
+
+        System.out.println("Welcome" + firstName + lastName + "\n");
+        PropertyManagersMainMenu();
+
+    }
+
+
+    public void StudentMainMenu(){
+        int command;
+        displayMenu(StudentMainMenu);
+        command = getCommand(StudentMainMenu.length);
+
+        switch (command){
+            case (0):
+                FindHouse();
+                break;
+
+            case (1):
+                SignLease();
+                break;
+
+            case (2):
+                LeaveReview();
+                break;
+
+            case (3):
+                StudentUserLogin();
+                break;
+
+            default:
+                System.out.println("Wrong choice!" + "\n");
+                break;
+        }
+        }
+
+
+
+
+    public void PropertyManagersMainMenu(){
+        int command;
+        displayMenu(PropertyManagers);
+        command = getCommand(PropertyManagers.length);
+
+        switch (command){
+            case (0):
+                UploadNewHouse();
+                break;
+            case (1):
+                SignLease();
+                break;
+
+            case (2):
+                PropertyManagersLogin();
+                break;
+
+            default:
+                System.out.println("Wrong choice!" + "\n");
+                break;
+        }
+        }
+
+     public void FindHouse(){
+        int command;
+        displayMenu(FindHouse);
+        command = getCommand(FindHouse.length);
+
+        switch (command){
+            case (0):
+                FindByTitle();
+                break;
+            case (1):
+                FindByType();
+                break;
+            case (2):
+                FindByAmenities();
+                break;
+
+            }
+        }
+
+        public void FindByName(){
+            System.out.println("Please Enter the Name of the Property" + "\n");
+            sc = new Scanner(System.in);
+            String propertyName = sc.nextLine();
+            System.out.println("\n" +"Searching......" + "\n");
+            HousingListing housingListing = HousingListing.getHousingListing;
+            if (propertyName == null)return;
+            if (housingListing.getListingName(propertyName)){
+                System.out.println();
+            }else {
+                System.out.println("Sorry, We don't have this property in our data base yet" + "\n");
+            }
+
+        }
+
+
+
+
+
+
+
 
 
 
