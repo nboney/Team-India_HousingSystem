7ea1a314ea06b82aea76388912875b8ec3996cd2
guzzarlk <70454751+guzzarlk@users.noreply.github.com>
2020-11-03 19:25:26 -0500

    Update LeaseAgreement.java

--- src/LeaseAgreement.java
+++ src/LeaseAgreement.java
@@ -5,7 +5,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.io.FileWriter;
 
-public class LeaseAgreement { //extends Document {
+public class LeaseAgreement { 
+	private static String text = "LANDLOARD";
 	private String landlord = "";
 	private String tenants = "";
 	private String propertyAddress;
@@ -18,50 +19,66 @@ public class LeaseAgreement { //extends Document {
     	private String paymentAddress;
     	private int damages;
     	private String tenant1 = "";
-
+    
     public LeaseAgreement() {
     	
     }
-	
  
-    public static void modifytxt(String fileName, String landlord, String replacement, String toFileName) throws IOException {
+    public static void modifytxt(String fileName, String text, String replacement, String toFileName) throws IOException {
     	
-    	//file should be local but I used this for now
+    	//file should be local
     	File oglease = new File("/Users/sonaguzzarlamudi/Desktop/LeaseAgreement.txt");
 		Scanner scanner = new Scanner(oglease);
 		FileWriter writer = new FileWriter("/Users/sonaguzzarlamudi/Desktop/NewLeaseAgreement.txt");
 		String line = "";
 		while(scanner.hasNextLine()) {
+			
+			read();
+			
 			line = scanner.nextLine();
-			line = line.replaceAll(landlord, replacement);
-			writer.write(line);
-			//writer.newLine();	
+			line = line.replaceAll(text, replacement);
+			writer.write(line);	
 		}
 		scanner.close();
 		writer.close();
     }
-	
+
+	private static void read() throws FileNotFoundException 
+	{
+		File oglease = new File("/Users/sonaguzzarlamudi/Desktop/LeaseAgreement.txt");
+		Scanner scanner = new Scanner(oglease);
+		if(scanner.hasNext("LANDLOARD")) {
+			//replace
+		}
+		else if (scanner.hasNext("TENANT")) {
+			
+		}
+		else if (scanner.hasNext("PROPERTY ADDRESS")) {
+			
+		}
+		
+		
+	}
+    
+    
+    
+	/*
     public static void main(String[] args) throws IOException {
     	
-    	System.out.println("What is the Landlord's name?");
-	Scanner keyboard = new Scanner(System.in);
-        String newLL = keyboard.nextLine();
+    	String[] text = {"LANDLOARD", "TENANT (s)"};
+    	
+    	for(int i = 0; i < text.length; i++) {
+    		
+    		System.out.println("What is the "+text[0]+" name?");
+    		Scanner keyboard = new Scanner(System.in);
+            String newLL = keyboard.nextLine();
+            
+            modifytxt("c:/Users/sonaguzzarlamudi/Desktop/LeaseAgreement.txt", ""+text+"", 
+            		" "+newLL+" ", "d:/Users/sonaguzzarlamudi/Desktop/NewLease.txt");
         
-    	modifytxt("c:/Users/sonaguzzarlamudi/Desktop/LeaseAgreement.txt", "LANDLOARD", " "+newLL+" ",
-    			"d:/Users/sonaguzzarlamudi/Desktop/NewLease.txt");
-        
-    	/*
-	this part isn't working out
-	
-    	System.out.println("What is the tenant's name?");
-        String newt = keyboard.nextLine();
-        String replacement = "";
-	replacement .replaceAll(newLL, newLL);
-        
-    	modifytxt("c:/Users/sonaguzzarlamudi/Desktop/LeaseAgreement.txt", "TENANT (s)", " "+newt+" ",
-    			"d:/Users/sonaguzzarlamudi/Desktop/NewLease.txt");
-    	*/
-    }
+    	}
+    
+    }*/
 }
 
     	
