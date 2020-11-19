08e07eda41467eb14411857afef2146e366e6113
Adam E <69927855+einsteia@users.noreply.github.com>
2020-11-18 19:17:06 -0500

    Don't need to test getters/setters

--- src/test/UsersTest.java
+++ src/test/UsersTest.java
@@ -3,7 +3,10 @@ package test.;
 import org.junit.Test; 
 import org.junit.Before; 
 import org.junit.After; 
-
+import java.util.ArrayList;
+import housingapplication2.pkg0.Users;
+import housingapplication2.pkg0.StudentUser;
+import static org.junit.Assert.*;
 /** 
 * Users Tester. 
 * 
@@ -13,163 +16,73 @@ import org.junit.After;
 */ 
 public class UsersTest { 
 
-@Before
-public void before() throws Exception { 
-} 
+    
+    @Before
+    public void before() throws Exception { 
+    } 
 
-@After
-public void after() throws Exception { 
-} 
+    @After
+    public void after() throws Exception { 
+    } 
 
-/** 
-* 
-* Method: getInstance() 
-* 
-*/ 
-@Test
-public void testGetInstance() throws Exception { 
-//TODO: Test goes here... 
-} 
+    /** 
+    * 
+    * Method: addStudent(StudentUser student) 
+    * 
+    */ 
+    @Test
+    public void testAddStudent() throws Exception { 
+       
+    } 
 
-/** 
-* 
-* Method: addStudent(StudentUser student) 
-* 
-*/ 
-@Test
-public void testAddStudent() throws Exception { 
-//TODO: Test goes here... 
-} 
+    /** 
+    * 
+    * Method: haveStudent(String username, int index) 
+    * 
+    */ 
+    @Test
+    public void testHaveStudent() throws Exception { 
+    //TODO: Test goes here... 
+    } 
 
-/** 
-* 
-* Method: haveStudent(String username, int index) 
-* 
-*/ 
-@Test
-public void testHaveStudent() throws Exception { 
-//TODO: Test goes here... 
-} 
+    /** 
+    * 
+    * Method: studentLogin(String username, String password) 
+    * 
+    */ 
+    @Test
+    public void testStudentLogin() throws Exception { 
+    //TODO: Test goes here... 
+    }
 
-/** 
-* 
-* Method: getStudent(String username) 
-* 
-*/ 
-@Test
-public void testGetStudent() throws Exception { 
-//TODO: Test goes here... 
-} 
+    /** 
+    * 
+    * Method: haveLeaser(String username, int index) 
+    * 
+    */ 
+    @Test
+    public void testHaveLeaser() throws Exception { 
+    //TODO: Test goes here... 
+    }
 
-/** 
-* 
-* Method: getStudentAtLogin(String username, String password) 
-* 
-*/ 
-@Test
-public void testGetStudentAtLogin() throws Exception { 
-//TODO: Test goes here... 
-} 
+    /** 
+    * 
+    * Method: addLeaser(LeasingUser leaser) 
+    * 
+    */ 
+    @Test
+    public void testAddLeaser() throws Exception { 
+    //TODO: Test goes here... 
+    } 
 
-/** 
-* 
-* Method: getStudentList() 
-* 
-*/ 
-@Test
-public void testGetStudentList() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: getLeaserList() 
-* 
-*/ 
-@Test
-public void testGetLeaserList() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: studentLogin(String username, String password) 
-* 
-*/ 
-@Test
-public void testStudentLogin() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: saveStudents() 
-* 
-*/ 
-@Test
-public void testSaveStudents() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: haveLeaser(String username, int index) 
-* 
-*/ 
-@Test
-public void testHaveLeaser() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: getLeaser(String username) 
-* 
-*/ 
-@Test
-public void testGetLeaser() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: addLeaser(LeasingUser leaser) 
-* 
-*/ 
-@Test
-public void testAddLeaser() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: leaserLogin(String username, String password) 
-* 
-*/ 
-@Test
-public void testLeaserLogin() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: getLeaserAtLogin(String username, String password) 
-* 
-*/ 
-@Test
-public void testGetLeaserAtLogin() throws Exception { 
-//TODO: Test goes here... 
-} 
-
-/** 
-* 
-* Method: saveLeasers() 
-* 
-*/ 
-@Test
-public void testSaveLeasers() throws Exception { 
-//TODO: Test goes here... 
-} 
-
+    /** 
+    * 
+    * Method: leaserLogin(String username, String password) 
+    * 
+    */ 
+    @Test
+    public void testLeaserLogin() throws Exception { 
+    //TODO: Test goes here... 
+    }
 
 } 
